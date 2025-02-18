eb2d382e41d6b761d4dc88933c97697c43f83e52
kabiraj khatri <kabirajkhatri@Mac.lan>
2025-02-18 21:19:23 +1100

    Basic version of the Recommendation app

--- app.py
+++ app.py
@@ -16,26 +16,37 @@ def home():
 # Define a route for search and recommendations
 @app.route('/recommend', methods=['POST'])
 def recommend():
-    # Get the movie query from the form
-    movie_query = request.form.get('movie')
+    movie_name = ""
+    recommendations = []
 
-    # API call to TMDb to search for movies
-    url = f"{TMDB_BASE_URL}/search/movie"
-    params = {
-        "api_key": TMDB_API_KEY,
-        "query": movie_query,
-        "language": "en-US"
-    }
-    response = requests.get(url, params=params)
+    #when user submits a movie title
+    if request.method == 'POST':
+        movie_name = request.form['movie']
 
-    if response.status_code == 200:
-        # Parse the API response
-        data = response.json()
-        # Get the top 10 movie titles
-        recommendations = [movie["title"] for movie in data.get("results", [])[:20]]
-        return render_template('recommendations.html', movie_name=movie_query, recommendations=recommendations)
-    else:
-        return f"Error: Unable to fetch data from TMDb. Status code: {response.status_code}"
+        url = f"{TMDB_BASE_URL}/search/movie?api_key={TMDB_API_KEY}&query={movie_name}"
+        response = requests.get(url)
+
+        if response.status_code == 200:  # Check if the API request was successful
+            data = response.json()  # Get the response data as a JSON object
+            if data['results']:
+                movie_id = data['results'][0]['id']  # Get the ID of the first result
+
+                # Step 2: Get similar movies based on the movie ID (API call)
+                recommendations_url = f"{TMDB_BASE_URL}/movie/{movie_id}/recommendations?api_key={TMDB_API_KEY}"
+                recommendations_response = requests.get(recommendations_url)
+                
+                if recommendations_response.status_code == 200:
+                    recommendations_data = recommendations_response.json()
+                    # Extract the movie titles from the response
+                    recommendations = []
+                    for movie in recommendations_data.get("results", []) [:2]:
+                        title = movie["title"]
+                        poster_path = movie.get("poster_path")
+                        poster_url =  poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}" if poster_path else None
+                        recommendations.append({"title":title, "poster_url":poster_url})
+
+    # Step 3: Render the HTML template with the movie name and recommendations
+    return render_template("recommendations.html", movie_name=movie_name, recommendations=recommendations)
 
 # Run the app
 if __name__ == '__main__':
